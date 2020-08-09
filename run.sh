22a28aecffbb751d97eb6f00e063f8a737971bbe
Gurbaaz Singh Nandra <60743677+gurbaaz27@users.noreply.github.com>
2020-08-09 12:00:22 +0530

    good code now

--- run.sh
+++ run.sh
@@ -1,21 +1,22 @@
-# echo $1
-# Assume file is in a folder.
-cd $1
-for f in *.mp3:
+#!/bin/sh
+# mp3 to wav conversion
+pushd $1
+for i in *.mp3:
 do 
-    ffmpeg -i "$f" -ar 16000 "../wav/${f%.*}.wav"
+    ffmpeg -i "$i" -ar 16000 "../wav/${i%.*}.wav"
 done
-cd ..
-# audio to wav done.
+popd
+# make dtln_out folder
 mkdir dtln_out/
-cd DTLN
+# DTLN processing
+pushd DTLN
 python3 run_evaluation.py -i ../wav/ -o ../dtln_out/ -m ./pretrained_model/model.h5
-cd ..
-# dtln outputs stored in dtln_out folder.
-cd Speaker-Diarization
+popd
+# Speaker Diaraization
+pushd Speaker-Diarization
 python3 speakerDiarization.py
-# speech diarized.
-cd ..
+popd
+# Transcription API
 python3 api.py
 
 
