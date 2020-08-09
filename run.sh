af73895223e9d96b116f2167a1c68ae1b5113273
Gurbaaz Singh Nandra <60743677+gurbaaz27@users.noreply.github.com>
2020-08-09 11:32:12 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,21 @@
 # echo $1
 # Assume file is in a folder.
-for f in $(ls $1):
+cd $1
+for f in *.mp3:
 do 
-    ffmpeg -i $1/$f -ar 16000 wav/$f.wav
+    ffmpeg -i "$f" -ar 16000 "../wav/${f%.*}.wav"
 done
+cd ..
 # audio to wav done.
+mkdir dtln_out/
+cd DTLN
+python3 run_evaluation.py -i ../wav/ -o ../dtln_out/ -m ./pretrained_model/model.h5
+cd ..
+# dtln outputs stored in dtln_out folder.
+cd Speaker-Diarization
+python3 speakerDiarization.py
+# speech diarized.
+cd ..
+python3 api.py
+
 
